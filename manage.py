9658cd155909202fbbfb77e9f6d095f0c99cc498
Gytha Ogg <gythaoggscat@gmail.com>
2025-07-30 10:10:46 +0200

    fix: point to correct settings and url

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apis_acdhch_default_settings.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apis_ontology.settings.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
